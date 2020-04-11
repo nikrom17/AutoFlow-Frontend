476727ad85e9e96ba368413336e5b9d1ddf7b13d
Nik <nikrom17@gmail.com>
2020-04-11 13:51:27 -0500

    add routing boilerplate code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,32 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import About from "./pages/aboutPage/aboutPage";
+import Home from "./pages/homePage/homePage";
 import "./App.css";
 
-const App: React.FC = () => <div className="App"></div>;
+const App: React.FC = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
