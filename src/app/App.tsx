200d44c26b31e8b4f07998efa75d546d94a8d9ff
Nik <nikrom17@gmail.com>
2020-07-14 09:33:28 -0500

    update import order

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
-import ErrorPage from "@pages/errorPage/errorPage";
-import Navigation from "@components/navigation/navigation";
-import Header from "@components/header/header";
 import { routes } from "src/app/routing/routeConstants";
+import ErrorPage from "@pages/errorPage/errorPage";
+import Header from "@components/header/header";
+import Navigation from "@components/navigation/navigation";
 import "./app.less";
 
 const App: React.FC = () => {
