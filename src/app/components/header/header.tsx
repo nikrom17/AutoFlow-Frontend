e214fdde0518ac24519b511390e70250edc6aed3
Nik <nikrom17@gmail.com>
2020-07-23 11:45:33 -0500

    add navigation buttons

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,34 +1,11 @@
 import React from "react";
 import { Button, Tooltip, Avatar, Badge } from "antd";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  SearchOutlined,
-  BellOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { NavContext } from "@components/navigation/context/navigation";
+import { SearchOutlined, BellOutlined, UserOutlined } from "@ant-design/icons";
 import * as styles from "./header.module.less";
 
 const Header = () => {
-  const {
-    navigationContext: { collapsed },
-    setNavigationContext,
-  } = React.useContext(NavContext);
-
   return (
     <div className={styles.header}>
-      {/* <Button
-        type="primary"
-        onClick={() => setNavigationContext({ collapsed: !collapsed })}
-        style={{ marginBottom: 16 }}
-        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-      ></Button> */}
-      <div className={styles.openCloseNav}>
-        <Tooltip title={collapsed ? "Expand Navigation" : "Close Navigation"}>
-          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        </Tooltip>
-      </div>
       <div className={styles.headerLeftSide}>
         <Tooltip title="Search">
           <div className={styles.headerChild}>
--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -1,66 +1,53 @@
 import React from "react";
-import { NavContext } from "@components/navigation/context/navigation";
+import { useHistory } from "react-router";
 import * as styles from "./navigation.module.less";
-import { Menu } from "antd";
-import {
-  MailOutlined,
-  CalendarOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-  LinkOutlined,
-} from "@ant-design/icons";
-
-const { SubMenu } = Menu;
+import { Tooltip, Button } from "antd";
+import { CarOutlined, ShopOutlined, PlusOutlined } from "@ant-design/icons";
 
 const Navigation = () => {
-  const {
-    navigationContext: { collapsed },
-  } = React.useContext(NavContext);
-
+  const history = useHistory();
   return (
     <div className={styles.navigation}>
-      <div className={styles.logo}>
-        <img
-          alt="logo"
-          src="https://cdn.canopytax.com/static/primary-navbar/canopy-logo-light.svg"
-        />
+      <div className={styles.navItem}>
+        <button className={styles.navButton} onClick={() => history.push("/")}>
+          <img
+            alt="logo"
+            src="https://cdn.canopytax.com/static/primary-navbar/canopy-logo-light.svg"
+          />
+        </button>
       </div>
+      <Tooltip title="Deliveries" placement="right">
+        <div className={styles.navItem}>
+          <button
+            className={styles.navButton}
+            onClick={() => history.push("/deliveries")}
+          >
+            <CarOutlined />
+          </button>
+        </div>
+      </Tooltip>
+      <Tooltip title="Locations" placement="right">
+        <div className={styles.navItem}>
+          <button
+            className={styles.navButton}
+            onClick={() => history.push("/locations")}
+          >
+            <ShopOutlined />
+          </button>
+        </div>
+      </Tooltip>
+      <Tooltip title="Create new" placement="right">
+        <div className={styles.navItem}>
+          <Button
+            icon={<PlusOutlined />}
+            shape="circle"
+            type="primary"
+            size="large"
+          ></Button>
+        </div>
+      </Tooltip>
     </div>
   );
 };
 
 export default Navigation;
-
-// <Menu
-//   defaultSelectedKeys={["1"]}
-//   defaultOpenKeys={["sub1"]}
-//   mode="vertical"
-//   theme="dark"
-//   inlineCollapsed={collapsed}
-// >
-//   <Menu.Item key="1" icon={<MailOutlined width={60} />}>
-//     Navigation One
-//   </Menu.Item>
-//   <Menu.Item key="2" icon={<CalendarOutlined />}>
-//     Navigation Two
-//   </Menu.Item>
-//   <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Navigation Two">
-//     <Menu.Item key="3">Option 3</Menu.Item>
-//     <Menu.Item key="4">Option 4</Menu.Item>
-//     <SubMenu key="sub1-2" title="Submenu">
-//       <Menu.Item key="5">Option 5</Menu.Item>
-//       <Menu.Item key="6">Option 6</Menu.Item>
-//     </SubMenu>
-//   </SubMenu>
-//   <SubMenu key="sub2" icon={<SettingOutlined />} title="Navigation Three">
-//     <Menu.Item key="7">Option 7</Menu.Item>
-//     <Menu.Item key="8">Option 8</Menu.Item>
-//     <Menu.Item key="9">Option 9</Menu.Item>
-//     <Menu.Item key="10">Option 10</Menu.Item>
-//   </SubMenu>
-//   <Menu.Item key="link" icon={<LinkOutlined />}>
-//     <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-//       Ant Design
-//     </a>
-//   </Menu.Item>
-// </Menu>
