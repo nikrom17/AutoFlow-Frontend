1ea2be806998a43bcbb3216716823bced2e04ae0
Nik <nikrom17@gmail.com>
2021-02-19 20:13:19 -0600

    add layout to app.tsx and use menu component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,29 +1,40 @@
-import * as React from "react";
-import { Route, Switch } from "react-router-dom";
+import * as React from 'react';
+import { Route, Switch } from 'react-router-dom';
 // import { withAuthenticationRequired } from "@auth0/auth0-react";
-import { routes } from "src/app/routing/routeConstants";
-import ErrorPage from "@pages/errorPage/errorPage";
-import Header from "@components/header/header";
-import Navigation from "@components/navigation/navigation";
-import "./app.less";
+import { Layout } from 'antd';
+import { routes } from 'src/app/routing/routeConstants';
+import ErrorPage from '@pages/errorPage/errorPage';
+import Headers from '@components/header/header';
+import Navigation from '@components/navigation/navigation';
+import './app.less';
+
+const { Header, Sider, Content } = Layout;
+const headerStyles = { paddingRight: '1rem', marginLeft: '0.1875rem' };
 
 const App: React.FC = () => {
+  // add things here
   return (
     <ErrorPage>
-      <Navigation />
-      <div className="wrapper">
-        <Header />
-        <main>
-          <Switch>
-            {routes.allIds.map((routeId) => {
-              const { Component, path } = routes.byId[routeId];
-              return (
-                <Route component={Component} exact key={path} path={path} />
-              );
-            })}
-          </Switch>
-        </main>
-      </div>
+      <Layout>
+        <Sider defaultCollapsed>
+          <Navigation />
+        </Sider>
+        <Layout>
+          <Header style={headerStyles}>
+            <Headers />
+          </Header>
+          <Content>
+            <main>
+              <Switch>
+                {routes.allIds.map((routeId) => {
+                  const { Component, path } = routes.byId[routeId];
+                  return <Route component={Component} exact key={path} path={path} />;
+                })}
+              </Switch>
+            </main>
+          </Content>
+        </Layout>
+      </Layout>
     </ErrorPage>
   );
 };
