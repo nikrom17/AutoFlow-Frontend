99b58d110c97c09002d22c8fc7ea90037a2499cd
Nik <nikrom17@gmail.com>
2021-03-06 20:40:18 -0600

    remove Auth0

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from './redux/rootReducer';
 import thunk from 'redux-thunk';
-import { Auth0Provider } from '@auth0/auth0-react';
-import auth0Config from '../auth0-config';
-// import * as Sentry from '@sentry/react';
-// import { Integrations as AmpIntegrations } from '@sentry/apm';
+import * as Sentry from '@sentry/react';
+import { Integrations as AmpIntegrations } from '@sentry/apm';
 import * as serviceWorker from './serviceWorker';
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
@@ -24,11 +22,12 @@ if (process.env.NODE_ENV === 'production') {
     email: 'nikolaus.j.roman@gamil.com',
   });
 
-  // Sentry.init({
-  //   dsn: 'https://56989b19084a4a0783d729f02cc0ff2a@o419182.ingest.sentry.io/5329556',
-  //   integrations: [new AmpIntegrations.Tracing()],
-  //   tracesSampleRate: 0.25,
-  // });
+  Sentry.init({
+    dsn: 'https://56989b19084a4a0783d729f02cc0ff2a@o419182.ingest.sentry.io/5329556',
+    // @ts-ignore
+    integrations: [new AmpIntegrations.Tracing()],
+    tracesSampleRate: 0.25,
+  });
 }
 
 declare global {
@@ -50,29 +49,13 @@ const store = createStore(
   composeEnhancers(applyMiddleware(thunk, LogRocket.reduxMiddleware()))
 );
 
-const onRedirectCallback = (appState: any) => {
-  // history.push(
-  //   appState && appState.returnTo
-  //     ? appState.returnTo
-  //     : window.location.pathname
-  // );
-  console.log(appState);
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={auth0Config.domain}
-      clientId={auth0Config.clientId}
-      redirectUri={auth0Config.redirect}
-      onRedirectCallback={onRedirectCallback}
-    >
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </Auth0Provider>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
