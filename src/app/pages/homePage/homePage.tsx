cdab3bb832ad033f223fb895c3a18f0779b397ba
Nik <nikrom17@gmail.com>
2021-03-07 09:08:36 -0600

    add statistic cards to home page

--- src/app/pages/homePage/homePage.tsx
+++ src/app/pages/homePage/homePage.tsx
@@ -1,24 +1,77 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 import PageFrame from '@components/pageFrame/pageFrame';
-import { Button } from 'antd';
+import { Row, Col, Card, Statistic, Divider, Calendar } from 'antd';
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
+import * as styles from './homePage.module.less';
 
 const Home: React.FC = () => {
-  const history = useHistory();
-  const { isAuthenticated, user } = useAuth0();
-
   return (
     <PageFrame>
-      <p>User Info:</p>
-      <p>{`First Name: ${isAuthenticated && user.given_name}`}</p>
-      <p>{`Last Name: ${isAuthenticated && user.family_name}`}</p>
-      <p>{`Email: ${isAuthenticated && user.email}`}</p>
-      <div>
-        <Button onClick={() => history.push('/about')} type="primary">
-          Go to the about page
-        </Button>
-      </div>
+      <h1 className={styles.home}>Home</h1>
+      <Divider />
+      <Row gutter={[24, 48]} className="rowStatistics">
+        <Col span={6}>
+          <Card>
+            <Row justify="space-between" align="bottom">
+              <Col span={12}>
+                <Statistic title="Total Leads" value={13} precision={0} />
+              </Col>
+              <Col span={5}>
+                <div className={styles.statisticPercentageUp}>
+                  <ArrowUpOutlined /> 4%
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <Row justify="space-between" align="bottom">
+              <Col span={12}>
+                <Statistic title="Total Todos" value={10} precision={0} />
+              </Col>
+              <Col span={5}>
+                <div className={styles.statisticPercentageUp}>
+                  <ArrowDownOutlined /> 1%
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <Row justify="space-between" align="bottom">
+              <Col span={12}>
+                <Statistic
+                  title="Opportunity Value"
+                  value={5000}
+                  precision={0}
+                  prefix="$"
+                />
+              </Col>
+              <Col span={5}>
+                <div className={styles.statisticPercentageUp}>
+                  <ArrowUpOutlined /> 11%
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <Row justify="space-between" align="bottom">
+              <Col span={12}>
+                <Statistic title="Conversion Rate" value={13} precision={0} suffix="%" />
+              </Col>
+              <Col span={5}>
+                <div className={styles.statisticPercentageDown}>
+                  <ArrowDownOutlined /> 5%
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     </PageFrame>
   );
 };
