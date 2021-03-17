53830a96a6f5a9dc93c773038487798c130d9547
Nik <nikrom17@gmail.com>
2021-03-17 08:29:42 -0500

    reduce width of todo modal

--- src/app/components/AddTodoModal/AddTodoModal.tsx
+++ src/app/components/AddTodoModal/AddTodoModal.tsx
@@ -12,8 +12,6 @@ const AddLeadModal: React.FC<Props> = ({ isModalVisible, setIsModalVisible }) =>
       visible={isModalVisible}
       footer={null}
       onCancel={() => setIsModalVisible(false)}
-      width="100%"
-      style={{ maxWidth: '1100px' }}
     >
       <p>This is the modal for adding Todos</p>
     </Modal>
